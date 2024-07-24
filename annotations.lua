57f31dc90c552c8b2ccf908401ef274a60f93241
Lucas Vienna <dev@lucasvienna.dev>
2024-07-24 07:18:09 +0200

    Initial commit

---
+++ annotations.lua
@@ -0,0 +1,4 @@
+---@meta
+
+---@class (exact) BBTWW
+---@field db table<string, table<number, number>>
---
+++ db.lua
@@ -0,0 +1,16 @@
+---@type string, BBTWW
+local _, addon = ...
+
+-- Database
+-----------------------------------------------------------
+addon.db = {
+	["|cff16B7FFRadiant Echoes|r"] = {
+		223641, -- Essence of Northrend
+		223642, -- Memory of Northrend
+		223643, -- Essence of Kalimdor
+		223644, -- Memory of Kalimdor
+		223645, -- Essence of the Eastern Kingdoms
+		223646, -- Memory of the Eastern Kingdoms
+		223647, -- Lifeless Stone Ring
+	}
+}
