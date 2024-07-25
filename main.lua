220bcc5dfe4254089412168ed1b87aca70331a82
Lucas Vienna <dev@lucasvienna.dev>
2024-07-25 07:01:07 +0200

    Wipe categories before repopulating

--- main.lua
+++ main.lua
@@ -8,6 +8,7 @@ local categories = BetterBags:GetModule('Categories')
 local L = BetterBags:GetModule('Localization')
 
 for category, items in pairs(addon.db) do
+	categories:WipeCategory(L:G(category))
 	for _, item in pairs(items) do
 		categories:AddItemToCategory(item, L:G(category))
 	end
