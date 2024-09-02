47bf8d13e51906902a6eb173ad3379436e0bd1d2
Lucas Vienna <dev@lucasvienna.dev>
2024-09-02 21:25:05 +0200

    Added BetterBags contexts

--- main.lua
+++ main.lua
@@ -5,16 +5,23 @@ local addon = select(2, ...)
 local BetterBags = LibStub('AceAddon-3.0'):GetAddon("BetterBags")
 
 ---@class Categories: AceModule
----@field WipeCategory fun(self: Categories, category: string)
----@field AddItemToCategory fun(self: Categories, item: number, category: string)
+---@field WipeCategory fun(self: Categories, ctx: Context, category: string)
+---@field AddItemToCategory fun(self: Categories, ctx: Context, item: number, category: string)
 local categories = BetterBags:GetModule('Categories')
+
 ---@class Localization: AceModule
 ---@field G fun(self: Localization, key: string): string
 local L = BetterBags:GetModule('Localization')
 
+---@class Context: AceModule
+---@field New fun(self: Context, name: string): Context
+local context = BetterBags:GetModule('Context')
+
+local ctx = context:New('BBTWW_Event')
+
 for category, items in pairs(addon.db) do
-	categories:WipeCategory(L:G(category))
+	categories:WipeCategory(ctx, L:G(category))
 	for _, item in pairs(items) do
-		categories:AddItemToCategory(item, L:G(category))
+		categories:AddItemToCategory(ctx, item, L:G(category))
 	end
 end
